0e7d6dee81a04e430bd47775d54c8a4f06b9018d
tbutler4 <terrencebutler@Mac-mini.attlocal.net>
2021-02-01 16:59:33 -0800

    feat(level) bronze submission

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # GA-Project-1
+
+Switch branches to view bronze/silver/gold submittions
+
+welcome to.. where.. this accomplishes this.. based on this.. my goal...
--- js/app.js
+++ js/app.js
@@ -29,3 +29,42 @@ const response = () => {
 }
 // calling fetch fucntion
 response()
+// looping through myData with setTimeout to insure data loading
+setTimeout(function(){ 
+  //looping over known amount of entries
+  for (let i = 0; i < 111; i++) {
+    // myData has no length so I must call bracket notation of i to reach entry
+    const data = myData[i];
+    //creating table for grabing and appending
+    const table = document.getElementById('table');
+    // creating table row for individual entries
+    const tableRow = document.createElement("TR");
+    // creating and supplying table data entries with information
+    const tableDataC = document.createElement("TD");
+    tableDataC.innerText = data.city
+    const tableDataD = document.createElement("TD");
+    tableDataD.innerText = data.date.slice(0,10)
+    const tableDataT = document.createElement("TD");
+    tableDataT.innerText = data.title
+    const tableDataL = document.createElement("TD");
+    let linkString = ''
+    for(let j = 0; j < data.links.length; j++){
+      const a = document.createElement("a");
+      a.setAttribute("href", data.links[j]);
+      linkString += (data.links[j] + '\n')
+    }
+    tableDataL.innerHTML = linkString
+    const tableDataTa = document.createElement("TD");
+    let tagString = ''
+    for(let j = 0; j < data.tags.length; j++){
+      tagString += (data.tags[j] + '\n')
+    }
+    tableDataTa.innerHTML = tagString
+    tableRow.appendChild(tableDataC);
+    tableRow.appendChild(tableDataD);
+    tableRow.appendChild(tableDataT);
+    tableRow.appendChild(tableDataL);
+    tableRow.appendChild(tableDataTa);
+    table.appendChild(tableRow);   
+  }
+}, 50);
